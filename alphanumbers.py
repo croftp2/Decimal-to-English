f2f7a8ce6d9aebd7c7059d1718f4611254d5da59
Paul Croft <pcroft@desktop.isilon.com>
2015-09-01 16:36:14 -0700

    Removed bug where numbers ending in 00 had the word "zero" in the end

--- alphanumbers.py
+++ alphanumbers.py
@@ -7,54 +7,91 @@ import sys
 
 #reserved numbers
 numbers = {
-	0:"zero",
-	1:"one",
-	2:"two",
-	3:"three",
-	4:"four",
-	5:"five",
-	6:"six",
-	7:"seven",
-	8:"eight",
-	9:"nine",
-	10:"ten",
-	11:"eleven",
-	12:"twelve",
-	13:"thirteen",
-	14:"fourteen",
-	15:"fifteen",
-	16:"sixteen",
-	17:"seventeen",
-	18:"eighteen",
-	19:"nineteen",
-	20:"twenty",
-	30:"thirty",
-	40:"fourty",
-	50:"fifty",
-	60:"sixty",
-	70:"seventy",
-	80:"eighty",
-	90:"ninety"}
+#    0:"zero",
+    0:"",
+    1:"one",
+    2:"two",
+    3:"three",
+    4:"four",
+    5:"five",
+    6:"six",
+    7:"seven",
+    8:"eight",
+    9:"nine",
+    10:"ten",
+    11:"eleven",
+    12:"twelve",
+    13:"thirteen",
+    14:"fourteen",
+    15:"fifteen",
+    16:"sixteen",
+    17:"seventeen",
+    18:"eighteen",
+    19:"nineteen",
+    20:"twenty",
+    30:"thirty",
+    40:"fourty",
+    50:"fifty",
+    60:"sixty",
+    70:"seventy",
+    80:"eighty",
+    90:"ninety"}
+
+placednumbers = {
+    1:"first",
+    2:"second",
+    3:"third",
+    5:"fifth",
+    8:"eighth",
+    9:"ninth",
+    12:"twelfth",
+    20:"twentieth",
+    30:"thirtieth",
+    40:"fourtieth",
+    50:"fiftieth",
+    60:"sixtieth",
+    70:"seventieth",
+    80:"eightieth",
+    90:"ninetieth",}
+
 
 places = (
-	(1000000000000,"trillion ",),
-	(1000000000,"billion ",),
-	(1000000,"million ",),
-	(1000,"thousand ",),
-	(100,"hundred ",),
-#	(10,""),
-	)
+    (1000000000000,"trillion",),
+    (1000000000,"billion",),
+    (1000000,"million",),
+    (1000,"thousand",),
+    (100,"hundred",),
+#   (10,""),
+    )
 
 def stringnumber(inint):
-	if inint < 0:
-		return "%s %s" % ("negative",stringnumber(inint * -1),)
-	if inint in numbers:
-		return numbers[inint]
-	for value, place in places:
-		if inint > value:
-			return "%s %s%s" % (stringnumber(inint / value),place, stringnumber(inint % value),)
-	#special case for unreserved numbers under 100
-	return "%s %s" % (stringnumber(inint - (inint % 10)),numbers[inint % 10],)
+    if inint < 0:
+        return "%s %s" % ("negative",stringnumber(inint * -1),)
+    if inint in numbers:
+        return numbers[inint]
+    for value, place in places:
+        if inint == value:
+            return place
+        elif inint > value:
+            return "%s %s %s" % (stringnumber(inint / value),place, stringnumber(inint % value),)
+    #special case for unreserved numbers under 100
+    return "%s %s" % (stringnumber(inint - (inint % 10)),numbers[inint % 10],)
+
+def placednumber(inint):
+    if inint < 0:
+        return "%s %s" % ("negative",placednumber(inint * -1),)
+    if inint in placednumbers:
+        return placednumbers[inint]
+    if inint in numbers:
+        return "%sth" % numbers[inint]
+    for value, place in places:
+        if inint == value:
+            return "%sth" % place
+        elif inint > value:
+            return "%s %s %s" % (stringnumber(inint / value),place, placednumber(inint % value),)
+
+    return "%s %sth" % (stringnumber(inint - (inint % 10)),numbers[inint % 10],)
+
 
 
 def main():
@@ -69,7 +106,10 @@ def main():
         exit(2)
 
     for i in inputs:
-         print stringnumber(i)
+        if i == 0:
+            print "zero"
+        else:
+            print stringnumber(i)
 
     return 0
 
